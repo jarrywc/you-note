f2e54b71a5a21503d45b161989b8077e29374ddd
jarrywc <66788651+jarrywc@users.noreply.github.com>
2022-03-28 01:11:12 -0400

    Create Flask app complete.

---
+++ app.py
@@ -0,0 +1,29 @@
+# pylint: disable=E1101
+# pylint: disable=C0413
+# pylint: disable=W1508
+# pylint: disable=R0903
+# pylint: disable=W0603
+import flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
+from flask_oauthlib.client import OAuth, OAuthException
+
+load_dotenv(find_dotenv())
+
+app = flask.Flask(__name__, static_folder="./build/static")
+# This tells our Flask app to look at the results of `npm build` instead of the
+db_url = os.getenv("DATABASE_URL")
+if db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+
+# If we add Google OAuth
+# app.config['GOOGLE_CLIENT_ID'] = os.getenv("GOOGLE_CLIENT_ID")
+# app.config['GOOGLE_CLIENT_SECRET'] = os.getenv("GOOGLE_CLIENT_SECRET")
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = os.environ.get("SECRET_KEY")
+oauth = OAuth(app)
+
+db = SQLAlchemy(app)
