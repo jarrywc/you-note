fb3c41c500040df4adc6e76a48638c7973ca6f3d
jarrywc <66788651+jarrywc@users.noreply.github.com>
2022-03-28 16:05:37 -0400

    Added Iframe div for external movie source

--- src/components/VideoInfo.js
+++ src/components/VideoInfo.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Iframe from 'react-iframe'
 
 export const VideoInfo = ({video}) => {
     const {Id, User_Id, Ext_Movie_Source, Title } = video || {};
@@ -10,6 +11,17 @@ export const VideoInfo = ({video}) => {
                 {User_Id}
                 {Ext_Movie_Source}
                 {Title}
+            </div>
+
+            <div>
+                {/*External Movie Source*/}
+                <Iframe url={Ext_Movie_Source}
+                        width="450px"
+                        height="450px"
+                        id="myId"
+                        className="myMovie"
+                        display="initial"
+                        position="relative"/>
             </div>
         </>
     ): <p>Video loading...</p>;
