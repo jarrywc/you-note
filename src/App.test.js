a4bef853090322a1a3d5e9d832612c64af458eb5
OchreVale <83403939+OchreVale@users.noreply.github.com>
2022-04-20 21:44:18 -0400

    Delete App.test.js
    Awaits the last version of App.js

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render} from '@testing-library/react';
-import App from './App';
-
-test("Testing App.js", () => {
-  const {getByText} = render(<App />);
-
-  const linkElement = getByText(/"search"/i);
-  expect(linkElement).toBeInTheDocument();
-});
